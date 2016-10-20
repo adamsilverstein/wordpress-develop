38a945a96f22cfc28606fd9e8868723255a4f443
Adam Silverstein <adam@10up.com>
2016-10-20 00:34:33 -0400

    add a filter for the dashboard

--- src/wp-includes/default-filters.php
+++ src/wp-includes/default-filters.php
@@ -494,5 +494,6 @@ add_filter( 'the_excerpt_embed',      'wp_embed_excerpt_attachment'           );
 add_filter( 'oembed_dataparse',       'wp_filter_oembed_result',        10, 3 );
 add_filter( 'oembed_response_data',   'get_oembed_response_data_rich',  10, 4 );
 add_filter( 'pre_oembed_result',      'wp_filter_pre_oembed_result',    10, 3 );
+add_action( 'rest_api_init',          'wp_dashboard_filter_api'              );
 
 unset( $filter, $action );
