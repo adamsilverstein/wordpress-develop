8ad8c5e144abf8966bced9c4cf6c6fc9cb5e1f3a
Adam Silverstein <adamjs@google.com>
2022-08-31 11:46:49 -0600

    `add_image_size`: add a new “disable_output_mapping” parameter

--- src/wp-includes/media.php
+++ src/wp-includes/media.php
@@ -278,23 +278,26 @@ function image_downsize( $id, $size = 'medium' ) {
  *
  * @global array $_wp_additional_image_sizes Associative array of additional image sizes.
  *
- * @param string     $name   Image size identifier.
- * @param int        $width  Optional. Image width in pixels. Default 0.
- * @param int        $height Optional. Image height in pixels. Default 0.
- * @param bool|array $crop   Optional. Image cropping behavior. If false, the image will be scaled (default),
- *                           If true, image will be cropped to the specified dimensions using center positions.
- *                           If an array, the image will be cropped using the array to specify the crop location.
- *                           Array values must be in the format: array( x_crop_position, y_crop_position ) where:
- *                               - x_crop_position accepts: 'left', 'center', or 'right'.
- *                               - y_crop_position accepts: 'top', 'center', or 'bottom'.
+ * @param string     $name                   Image size identifier.
+ * @param int        $width                  Optional. Image width in pixels. Default 0.
+ * @param int        $height                 Optional. Image height in pixels. Default 0.
+ * @param bool|array $crop                   Optional. Image cropping behavior. If false, the image will be scaled (default),
+ *                                           If true, image will be cropped to the specified dimensions using center positions.
+ *                                           If an array, the image will be cropped using the array to specify the crop location.
+ *                                           Array values must be in the format: array( x_crop_position, y_crop_position ) where:
+ *                                               - x_crop_position accepts: 'left', 'center', or 'right'.
+ *                                               - y_crop_position accepts: 'top', 'center', or 'bottom'.
+ * @param bool       $disable_output_mapping Optional. Whether to disable output format mapping. Default false.
  */
-function add_image_size( $name, $width = 0, $height = 0, $crop = false ) {
+ */
+function add_image_size( $name, $width = 0, $height = 0, $crop = false, $disable_output_mapping = false ) {
 	global $_wp_additional_image_sizes;
 
 	$_wp_additional_image_sizes[ $name ] = array(
-		'width'  => absint( $width ),
-		'height' => absint( $height ),
-		'crop'   => $crop,
+		'width'                  => absint( $width ),
+		'height'                 => absint( $height ),
+		'crop'                   => $crop,
+		'disable_output_mapping' => $disable_output_mapping,
 	);
 }
 
