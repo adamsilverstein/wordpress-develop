1f16c40081671af294bd02d2fe1a799f08ff6698
adamsilverstein <adamsilverstein@earthboundhosting.com>
2025-11-07 09:22:04 -0700

    By default, only notify for approved comments and notes

--- src/wp-includes/comment.php
+++ src/wp-includes/comment.php
@@ -2429,6 +2429,13 @@ function wp_new_comment_notify_postauthor( $comment_id ) {
 
 	$maybe_notify = $is_note ? get_option( 'wp_notes_notify' ) : get_option( 'comments_notify' );
 
+	// By default, only notify for approved comments and notes.
+	if (
+		! isset( $comment->comment_approved ) ||
+		( '1' !== $comment->comment_approved && ! $is_note ) ) {
+			$maybe_notify = false;
+	}
+
 	/**
 	 * Filters whether to send the post author new comment notification emails,
 	 * overriding the site setting.
@@ -2446,13 +2453,6 @@ function wp_new_comment_notify_postauthor( $comment_id ) {
 	 */
 	if ( ! $maybe_notify ) {
 		return false;
-	}
-
-	// Send notifications for approved comments and all notes.
-	if (
-		! isset( $comment->comment_approved ) ||
-		( '1' !== $comment->comment_approved && ! $is_note ) ) {
-			return false;
 	}
 
 	return wp_notify_postauthor( $comment_id );
